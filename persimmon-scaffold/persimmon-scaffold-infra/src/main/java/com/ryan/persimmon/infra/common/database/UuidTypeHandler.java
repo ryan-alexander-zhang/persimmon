fefb7fb8778a822e7928265215bdf195227a37be
ryan-alexander-zhang <ryan.alexander.zhang@gmail.com>
2026-01-30 16:36:39 +0800

    feat: add UuidTypeHandler for handling UUID parameters in MyBatis

---
+++ persimmon-scaffold/persimmon-scaffold-infra/src/main/java/com/ryan/persimmon/infra/common/database/UuidTypeHandler.java
@@ -0,0 +1,37 @@
+package com.ryan.persimmon.infra.common.database;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+@MappedTypes(UUID.class)
+@MappedJdbcTypes(JdbcType.OTHER)
+public class UuidTypeHandler extends BaseTypeHandler<UUID> {
+
+  @Override
+  public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType)
+      throws SQLException {
+    ps.setObject(i, parameter);
+  }
+
+  @Override
+  public UUID getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    return (UUID) rs.getObject(columnName);
+  }
+
+  @Override
+  public UUID getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    return (UUID) rs.getObject(columnIndex);
+  }
+
+  @Override
+  public UUID getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    return (UUID) cs.getObject(columnIndex);
+  }
+}
