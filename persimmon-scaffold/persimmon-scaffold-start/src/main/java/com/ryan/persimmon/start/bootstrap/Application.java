4843d3cf2b13d07675d42e66e729e6ffb8744dab
ryan-alexander-zhang <ryan.alexander.zhang@gmail.com>
2026-02-02 11:09:36 +0800

    feat: add MybatisMapperScanConfig for custom mapper scanning and update Application class

--- persimmon-scaffold/persimmon-scaffold-start/src/main/java/com/ryan/persimmon/start/bootstrap/Application.java
+++ persimmon-scaffold/persimmon-scaffold-start/src/main/java/com/ryan/persimmon/start/bootstrap/Application.java
@@ -3,7 +3,8 @@ package com.ryan.persimmon.start.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {"com.ryan.persimmon.start.config", "com.ryan.persimmon.start.profile"})
 public class Application {
 
   public static void main(String[] args) {
---
+++ persimmon-scaffold/persimmon-scaffold-start/src/main/java/com/ryan/persimmon/start/config/scan/MybatisMapperScanConfig.java
@@ -0,0 +1,16 @@
+package com.ryan.persimmon.start.config.scan;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+@MapperScan(
+    basePackages = {"com.ryan.persimmon.infra.repository", "com.ryan.persimmon.infra.query"})
+public class MybatisMapperScanConfig {
+
+  public MybatisMapperScanConfig() {
+    log.info("MybatisMapperScanConfig init");
+  }
+}
